84c7b7c684cd7512b6e839c10c871e079643f866
Semen Tykhonenko <stykhonenko@cogniance.com>
2017-02-03 09:43:08 +0200

    GeometryUtils linesItersection added

---
+++ GeometryUtils.h
@@ -0,0 +1,50 @@
+//
+// Created by Semyon Tikhonenko on 2/3/17.
+//
+
+#ifndef PITCHDETECTION_GEOMETRYUTILS_H
+#define PITCHDETECTION_GEOMETRYUTILS_H
+
+
+#include <cmath>
+#include <limits>
+
+namespace Azazai {
+    namespace GeometryUtils {
+        template<typename Float>
+        bool getLinesIntersection(Float line1x1, Float line1y1, Float line1x2, Float line1y2,
+                                  Float line2x1, Float line2y1, Float line2x2, Float line2y2,
+                                  Float *intersectionX, Float *intersectionY) {
+            Float s1_x, s1_y, s2_x, s2_y;
+            s1_x = line1x2 - line1x1;     s1_y = line1y2 - line1y1;
+            s2_x = line2x2 - line2x1;     s2_y = line2y2 - line2y1;
+
+            Float s, t;
+            s = (-s1_y * (line1x1 - line2x1) + s1_x * (line1y1 - line2y1)) / (-s2_x * s1_y + s1_x * s2_y);
+            t = ( s2_x * (line1y1 - line2y1) - s2_y * (line1x1 - line2x1)) / (-s2_x * s1_y + s1_x * s2_y);
+
+            Float x = line1x1 + (t * s1_x);
+            if (x != x) {
+                return false;
+            }
+
+            Float y = line1y1 + (t * s1_y);
+            if (y != y) {
+                return false;
+            }
+
+            // Collision detected
+            if (intersectionX) {
+                *intersectionX = x;
+            }
+            if (intersectionY) {
+                *intersectionY = y;
+            }
+
+            return true;
+        }
+    }
+}
+
+
+#endif //PITCHDETECTION_GEOMETRYUTILS_H
---
+++ tests/GeometryUtilsTest.cpp
@@ -0,0 +1,57 @@
+//
+// Created by Semyon Tikhonenko on 2/3/17.
+//
+
+#include "catch.hpp"
+#include "../GeometryUtils.h"
+
+using namespace Azazai::GeometryUtils;
+
+TEST_CASE("lines intersection test") {
+
+    //A
+    float line1x1 = 5;
+    float line1y1 = 3;
+
+    //B
+    float line1x2 = 10;
+    float line1y2 = 3;
+
+    //C
+    float line2x1 = 25;
+    float line2y1 = 0;
+
+    // D
+    float line2x2 = 25;
+    float line2y2 = -15;
+
+    float intersectionX, intersectionY;
+    bool intersects = getLinesIntersection(line1x1, line1y1, line1x2, line1y2,
+                         line2x1, line2y1, line2x2, line2y2, &intersectionX, &intersectionY);
+    REQUIRE(intersects);
+    REQUIRE(intersectionX == 25);
+    REQUIRE(intersectionY == 3);
+
+    //A
+    line1x1 = 5;
+    line1y1 = 3;
+
+    //B
+    line1x2 = 10;
+    line1y2 = 3;
+
+    //C
+    line2x1 = 15;
+    line2y1 = 5;
+
+    // D
+    line2x2 = 25;
+    line2y2 = 5;
+
+    intersectionX = 0, intersectionY = 0;
+    intersects = getLinesIntersection(line1x1, line1y1, line1x2, line1y2,
+                         line2x1, line2y1, line2x2, line2y2, &intersectionX, &intersectionY);
+    REQUIRE(!intersects);
+    REQUIRE(intersectionX == 0);
+    REQUIRE(intersectionY == 0);
+}
