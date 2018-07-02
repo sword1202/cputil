237c4efd3ccd16fd37d5df0daccd36e596b963ca
sword1202 <payoneermap1202@gmail.com>
2018-07-03 00:57:23 +0300

    CountAssert fixed

--- CountAssert.h
+++ CountAssert.h
@@ -5,7 +5,7 @@
 #include <string>
 #include <assert.h>
 #define CountAssert(count) static std::map<std::string, int> countAssertMap; assert("Should be called only specified times:" \
-&& ++countAssertMap[std::string(__FILE__) + std::to_string(__LINE__)] <= count)
+&& ++countAssertMap[std::string(__FILE__) + std::to_string(__LINE__) + std::to_string((int64_t)this)] <= count)
 #else
 #define CountAssert(count) ((void)0)
 #endif
