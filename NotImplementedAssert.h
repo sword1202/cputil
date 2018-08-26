b44562b5fdbe53f2e3dff92247ea77d92a65d8bb
sword1202 <payoneermap1202@gmail.com>
2018-08-26 02:39:55 -0300

    Not implemented assert added

---
+++ NotImplementedAssert.h
@@ -0,0 +1,13 @@
+//
+// Created by Semyon Tikhonenko on 8/26/18.
+// Copyright (c) 2018 Mac. All rights reserved.
+//
+
+#ifndef NOT_IMPLEMENTED_ASSERT
+
+#include <assert.h>
+
+#define NOT_IMPLEMENTED_ASSERT assert(false && "Not implemented");
+
+
+#endif //VOCALTRAINER_NOTIMPLEMENTEDMACRO_H
