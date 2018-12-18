3cb741508b49c142dbba98b688dd0344f2f22b13
sword1202 <payoneermap1202@gmail.com>
2018-12-18 15:04:50 +0200

    playhead clock integrated

--- TimeUtils.h
+++ TimeUtils.h
@@ -11,12 +11,29 @@
 #include <iostream>
 #include <unordered_map>
 #include <string>
+#include <iomanip>
+#include <sstream>
 
 namespace CppUtils {
 namespace TimeUtils {
 	int64_t NowInMicroseconds();
 	std::string NowInMicrosecondsInPrettyFormat();
 	double NowInSeconds();
+
+	// Uses std::put_time format
+	template<typename Char, typename Stream>
+	void WriteFormattedTimeToStream(const Char* format, int timeInSeconds, Stream& out) {
+		time_t asTimeT = timeInSeconds;
+		auto time = *std::localtime(&asTimeT);
+		out << std::put_time(&time, format);
+	}
+
+	template<typename Char>
+	std::basic_string<Char> ConvertSecondsToFormattedTimeString(const Char* format, int timeInSeconds) {
+		std::basic_stringstream<Char> stream;
+		WriteFormattedTimeToStream(format, timeInSeconds, stream);
+		return stream.str();
+	}
 
 	template <typename Func, typename Stream = std::ostream>
 	void LogExecutionTime(const char* label, Func func, Stream& os = std::cout) {
