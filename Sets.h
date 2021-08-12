5b3cfe407ff854bd658f81f7541178d5f24bff88
Semyon Tikhonenko <v.semyon.tikhonenko@reddit.com>
2021-08-12 16:17:46 +0300

    VocalPartAudioDataGenerator optimized using intervals

--- Sets.h
+++ Sets.h
@@ -7,23 +7,24 @@
 #define VOCALTRAINER_SETS_H
 
 #include <algorithm>
+#include <set>
 
 namespace CppUtils {
     namespace Sets {
-        template <typename Set>
-        Set Difference(const Set& a, const Set& b) {
-            Set result;
+        template <typename Set1, typename Set2, typename Result = Set1>
+        Result Difference(const Set1& a, const Set2& b) {
+            Result result;
             std::set_difference(a.begin(), a.end(), b.begin(), b.end(), std::inserter(result, result.end()));
             return result;
         }
 
-        template <typename Set, typename OutContainer>
-        void Difference(const Set& a, const Set& b, OutContainer* result) {
+        template <typename Set1, typename Set2, typename OutContainer>
+        void Difference(const Set1& a, const Set2& b, OutContainer* result) {
             std::set_difference(a.begin(), a.end(), b.begin(), b.end(), std::inserter(*result, result->end()));
         }
 
-        template <typename Set, typename OutIterator>
-        void Difference(const Set& a, const Set& b, OutIterator outIterator) {
+        template <typename Set1, typename Set2, typename OutIterator>
+        void Difference(const Set1& a, const Set2& b, OutIterator outIterator) {
             std::set_difference(a.begin(), a.end(), b.begin(), b.end(), outIterator);
         }
 
@@ -46,6 +47,12 @@ namespace CppUtils {
                     set.erase(iter++);
                 }
             }
+        }
+
+        template <typename T>
+        const std::set<T>& emptySet() {
+            static std::set<T> s;
+            return s;
         }
     }
 };
